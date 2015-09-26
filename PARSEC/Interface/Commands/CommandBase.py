5f03d7de62908ffae3884ff005c41489410cee4f
Byte Commander <bytecommander@gmx.de>
2015-09-26 18:21:42 +0200

    Write CommandBaseClassWithEventSwitch
    Derived from CommandBaseClass, but checks parameter types and redirects all calls to different methods depending on the event.event_type value.

--- PARSEC/Interface/Commands/CommandBase.py
+++ PARSEC/Interface/Commands/CommandBase.py
@@ -1,10 +1,45 @@
 __author__ = 'ByteCommander'
 from PARSEC.Interface import _default_implementation
+from PARSEC.Interface.Events import EventBase
+from PARSEC.Interface.Commands import CommandAPI
 
 
-class CommandClassBase(object):
+class CommandBaseClass(object):
 
     def on_event(self, event, parsec_interface):
         _default_implementation(self, "on_event")
 
 
+class CommandBaseClassWithEventSwitch(CommandBaseClass):
+
+    def on_event(self, event, parsec_interface):
+        if not issubclass(type(event), EventBase.EventBaseClass):
+            raise TypeError("'on_event' requires as 'event' argument an instance of a subclass of "
+                            "'PARSEC.Interface.Events.EventBase.EventBaseClass'!")
+        if not isinstance(type(parsec_interface), CommandAPI.ParsecInterface):
+            raise TypeError("'on_event' requires as 'parsec_interface' argument an instance of "
+                            "'PARSEC.Interface.Commands.CommandAPI.ParsecInterface'!")
+
+        return (getattr(self, "on_{s}_event".format(event.event_type.lower()), self.on_default_event)
+                )(self, event, parsec_interface)
+
+    def on_default_event(self, event, parsec_interface):
+        _default_implementation(self, "on_default_event")
+
+    def on_message_event(self, event, parsec_interface):
+        _default_implementation(self, "on_message_event")
+
+    def on_ping_event(self, event, parsec_interface):
+        _default_implementation(self, "on_ping_event")
+
+    def on_reply_event(self, event, parsec_interface):
+        _default_implementation(self, "on_reply_event")
+
+    def on_command_event(self, event, parsec_interface):
+        _default_implementation(self, "on_command_event")
+
+    def on_room_event(self, event, parsec_interface):
+        _default_implementation(self, "on_room_event")
+
+    def on_user_event(self, event, parsec_interface):
+        _default_implementation(self, "on_user_event")
