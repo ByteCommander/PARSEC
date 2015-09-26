f3a2570f0b607d024860fe13de463dcb2d811173
Byte Commander <bytecommander@gmx.de>
2015-09-26 14:00:31 +0200

    Write _default_implementation(...) for abstract base classes
     Function _default_implementation(instance, method_name) will raise a NotImplementedError with appropriate message containing invoked method name, class module and name and superclass.

--- PARSEC/Interface/CommandAPI/BaseClasses.py
+++ PARSEC/Interface/CommandAPI/BaseClasses.py
@@ -1,1 +1,9 @@
 __author__ = 'ByteCommander'
+
+
+def _default_implementation(instance, method_name):
+    raise NotImplementedError("Function '{s}' not implemented by class '{s}.{s}' derived from {s}!".format(
+        method_name, instance.__class__.__module__, instance.__class__.__name__, instance.__class__.__base__.__name__
+    ))
+
+
