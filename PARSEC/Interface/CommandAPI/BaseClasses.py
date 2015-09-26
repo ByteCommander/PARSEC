67afb93621a4fc796d2cbfe5cec22a6858404f7b
Byte Commander <bytecommander@gmx.de>
2015-09-26 14:20:11 +0200

    Move _default_implementation(...) to __init__.py of package Interface

--- PARSEC/Interface/CommandAPI/BaseClasses.py
+++ PARSEC/Interface/CommandAPI/BaseClasses.py
@@ -1,14 +1,10 @@
 __author__ = 'ByteCommander'
-
-
-def _default_implementation(instance, method_name):
-    raise NotImplementedError("Function '{s}' not implemented by class '{s}.{s}' derived from {s}!".format(
-        method_name, instance.__class__.__module__, instance.__class__.__name__, instance.__class__.__base__.__name__
-    ))
+from PARSEC.Interface import _default_implementation
 
 
 class CommandClassBase(object):
 
     def on_event(self, event, parsec_interface):
         _default_implementation(self, "on_event")
+
 
